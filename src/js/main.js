cff4d554f5f3817c34e8f5f1be6d5372e7b58ad4
ytyaru <yttry0@gmail.com>
2021-12-28 15:22:34 +0900

    Created a repository

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    initFontSize();
+    console.log("DOMContentLoaded");
+    console.log(`devicePixelRatio = ${window.devicePixelRatio}`);
+;
+});
+window.addEventListener('load', (event) => {
+    console.log("load");
+});
+
